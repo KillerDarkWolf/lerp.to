fa3fcb9f629a74afa4d303c5a781690ccdfd5802
KillerDarkWolf <tristancc99@gmail.com>
2020-10-25 13:33:54 -0400

    finished backend

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const path = require('path');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const yup = require('yup');
+const monk = require('monk');
+const { nanoid } = require('nanoid');
+
+const db = monk(
+  'mongodb+srv://admin:Dinodab99@lerpto.p6rxh.mongodb.net/lerpto?retryWrites=true&w=majority',
+);
+const urls = db.get('shortUrls');
+urls.createIndex({ slug: 1 }, { unique: true });
 
 const app = express();
 
@@ -19,8 +28,68 @@ app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/.*', (req, res, next) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/:id', async (req, res, next) => {
+  const { id: slug } = req.params;
+  try {
+    const url = await urls.findOne({ slug });
+    if (url) {
+      res.redirect(url.url);
+    }
+    res.redirect(`/?error=${slug} not found`);
+  } catch (error) {
+    res.redirect(`/?error=Link not found`);
+  }
+});
+
+const schema = yup.object().shape({
+  alias: yup
+    .string()
+    .trim()
+    .matches(/[\w\-]/i),
+  url: yup.string().trim().url().required(),
+});
+
+app.post('/url', async (req, res, next) => {
+  let { slug, url } = req.body;
+  try {
+    if (!slug) {
+      slug = nanoid(4);
+    } else {
+      const exisiting = await urls.findOne({ slug });
+      if (exisiting) {
+        throw new Error('Slug in use. 🐌');
+      }
+    }
+    await schema.validate({
+      slug,
+      url,
+    });
+    slug = slug.toLowerCase();
+    let newUrl = {
+      url,
+      slug,
+    };
+    const created = await urls.insert(newUrl);
+    res.json(created);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get('/url/:id', (req, res, next) => {
+  // get url info by id
+});
+
+app.use((error, req, res, next) => {
+  if (error.status) {
+    res.status(error.status);
+  } else {
+    res.status(500);
+  }
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
+  });
 });
 
 const port = process.env.PORT || 3000;
